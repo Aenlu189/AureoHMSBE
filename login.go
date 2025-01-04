70878ae3619891121f0f2757f1c8c32f690809af
Aen Lu <aenlu189@gmail.com>
2025-01-04 03:46:09 +0000

    "Log in feature added"

--- login.go
+++ login.go
@@ -1,11 +1,80 @@
-package AureoHMSBE
+package main
 
 import (
 	"errors"
+	"fmt"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
 	"net/http"
 )
 
-func main() {
+type receptionist struct {
+	ID       int    `gorm:"primaryKey"`
+	Name     string `gorm:"not null"`
+	Email    string `gorm:"not null"`
+	Username string `gorm:"unique: not null"`
+	Password string `gorm:"not null"`
+}
 
+var db *gorm.DB
+
+func main() {
+	dsn := "root:Hninhninlatt21@tcp(127.0.0.1:3306)/Aureo_Cloud?charset=utf8mb4&parseTime=True&loc=Local"
+	var err error
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to the database: %v", err)
+	}
+
+	dbError := db.AutoMigrate(&receptionist{})
+	if dbError != nil {
+		return
+	}
+	fmt.Println("Database and tables created successfully")
+
+	router := gin.Default()
+
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"*"}
+	router.Use(cors.New(config))
+
+	router.POST("/login", login)
+
+	runErr := router.Run("localhost:8080")
+	if runErr != nil {
+		fmt.Printf("Localhost server not running")
+		return
+	}
+}
+
+func login(c *gin.Context) {
+	var requestData struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+
+	if err := c.BindJSON(&requestData); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Invalid request body"})
+		return
+	}
+
+	var user receptionist
+	result := db.Where("username = ?", requestData.Username).First(&user)
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			c.JSON(http.StatusNotFound, gin.H{"message": "Invalid username or password"})
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{"message": result.Error.Error()})
+		}
+		return
+	}
+
+	if user.Password != requestData.Password {
+		c.JSON(http.StatusUnauthorized, gin.H{"message": "Invalid username or password"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Login successful"})
 }
